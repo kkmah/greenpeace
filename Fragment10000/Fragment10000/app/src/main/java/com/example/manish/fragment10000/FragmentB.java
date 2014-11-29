76491c09d3cb51c29b62cb2dc0c148e6e4acac89
Kailash Mahabier <kkmah7@gmail.com>
2014-11-29 22:55:04 +0100

    Fragment project

---
+++ Fragment10000/Fragment10000/app/src/main/java/com/example/manish/fragment10000/FragmentB.java
@@ -0,0 +1,20 @@
+package com.example.manish.fragment10000;
+
+/**
+ * Created by Manish on 29-11-2014.
+ */
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Vivz on 10/25/13.
+ */
+public class FragmentB extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+}
